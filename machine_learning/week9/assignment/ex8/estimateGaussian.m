29e00c162ea7a3f06e929efb960427c75c15fcea
peroon <fromjpn@gmail.com>
2016-09-13 00:13:05 +0900

    week 9: completed up to Anomaly Detection (30/100)

--- machine_learning/week9/assignment/ex8/estimateGaussian.m
+++ machine_learning/week9/assignment/ex8/estimateGaussian.m
@@ -1,14 +1,16 @@
 function [mu sigma2] = estimateGaussian(X)
-%ESTIMATEGAUSSIAN This function estimates the parameters of a 
+%ESTIMATEGAUSSIAN This function estimates the parameters of a
 %Gaussian distribution using the data in X
-%   [mu sigma2] = estimateGaussian(X), 
+%   [mu sigma2] = estimateGaussian(X),
 %   The input X is the dataset with each n-dimensional data point in one row
 %   The output is an n-dimensional vector mu, the mean of the data set
 %   and the variances sigma^2, an n x 1 vector
-% 
+%
 
 % Useful variables
 [m, n] = size(X);
+% m: #data
+% n: #feature
 
 % You should return these values correctly
 mu = zeros(n, 1);
@@ -21,16 +23,10 @@ sigma2 = zeros(n, 1);
 %               should contain variance of the i-th feature.
 %
 
+mu = mean(X, 1);
 
-
-
-
-
-
-
-
+sigma2 = (1/m) * sum( (X - mu).^2 , 1);
 
 % =============================================================
-
 
 end
--- machine_learning/week9/assignment/ex8/selectThreshold.m
+++ machine_learning/week9/assignment/ex8/selectThreshold.m
@@ -6,34 +6,48 @@ function [bestEpsilon bestF1] = selectThreshold(yval, pval)
 %   validation set (pval) and the ground truth (yval).
 %
 
+% yval: 0/1ラベル
+% pval: 確率
+
 bestEpsilon = 0;
 bestF1 = 0;
 F1 = 0;
 
-stepsize = (max(pval) - min(pval)) / 1000;
+%stepnum = 10; %for debug
+stepnum = 1000;
+
+stepsize = (max(pval) - min(pval)) / stepnum;
+
+% εを変えながら
 for epsilon = min(pval):stepsize:max(pval)
-    
+
     % ====================== YOUR CODE HERE ======================
     % Instructions: Compute the F1 score of choosing epsilon as the
     %               threshold and place the value in F1. The code at the
     %               end of the loop will compare the F1 score for this
     %               choice of epsilon and set it to be the best epsilon if
     %               it is better than the current choice of epsilon.
-    %               
+    %
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
+    %予測する
+    is_predicted_anomaly = pval < epsilon; % n x 1
 
+    %anomalyと判定して、正解もanomaly
+    true_positive = sum((is_predicted_anomaly==1) & (yval==1));
 
+    %anomalyと判定。正解はanomalyじゃない
+    false_positive = sum((is_predicted_anomaly==1) & (yval==0));
 
+    %anomalyなのにnot anomalyと判定してしまった
+    false_negative = sum((is_predicted_anomaly==0) & (yval==1));
 
+    %F1を求めるための準備
+    prec = true_positive / (true_positive + false_positive);
+    rec = true_positive / (true_positive + false_negative);
 
-
-
-
-
-
-
+    F1 = 2 * prec * rec / (prec + rec);
 
     % =============================================================
 
