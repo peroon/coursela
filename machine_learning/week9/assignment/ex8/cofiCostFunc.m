1f1f0436f5675391bbed373a5bcdbfa24a173931
peroon <fromjpn@gmail.com>
2016-09-13 20:39:15 +0900

    week 9: complete programming assignment (Collaborative Filtering) (100/100)

--- machine_learning/week9/assignment/ex8/cofiCostFunc.m
+++ machine_learning/week9/assignment/ex8/cofiCostFunc.m
@@ -6,12 +6,16 @@ function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
 %   collaborative filtering problem.
 %
 
+% paramsの前半
 % Unfold the U and W matrices from params
 X = reshape(params(1:num_movies*num_features), num_movies, num_features);
+% num_movies x num_featuresの行列
+
+% paramsの後半
 Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
 
-            
+
 % You need to return the following values correctly
 J = 0;
 X_grad = zeros(size(X));
@@ -21,7 +25,7 @@ Theta_grad = zeros(size(Theta));
 % Instructions: Compute the cost function and gradient for collaborative
 %               filtering. Concretely, you should first implement the cost
 %               function (without regularization) and make sure it is
-%               matches our costs. After that, you should implement the 
+%               matches our costs. After that, you should implement the
 %               gradient and use the checkCostFunction routine to check
 %               that the gradient is correct. Finally, you should implement
 %               regularization.
@@ -29,31 +33,37 @@ Theta_grad = zeros(size(Theta));
 % Notes: X - num_movies  x num_features matrix of movie features
 %        Theta - num_users  x num_features matrix of user features
 %        Y - num_movies x num_users matrix of user ratings of movies
-%        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
+%        R - num_movies x num_users matrix, where R(i, j) = 1 if the
 %            i-th movie was rated by the j-th user
 %
 % You should set the following variables correctly:
 %
-%        X_grad - num_movies x num_features matrix, containing the 
+%        X_grad - num_movies x num_features matrix, containing the
 %                 partial derivatives w.r.t. to each element of X
-%        Theta_grad - num_users x num_features matrix, containing the 
+%        Theta_grad - num_users x num_features matrix, containing the
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+%コスト
+J = 1/2 * sum(sum(        (Theta * X' - Y').^2      .* R'      ));
 
+prediction_matrix = X * Theta'; % n_m x n * n x n_u = n_m x n_u
+diff_matrix = prediction_matrix - Y;
+diff_matrix_excluded = diff_matrix .* R; %r(i,j)==0の値を0にして除去したもの
 
+% n_m x n
+X_grad = diff_matrix_excluded * Theta; % n_m x n_u * n_u x n = n_m x n
 
+% n_u x n
+Theta_grad = diff_matrix_excluded' * X; % n_u x n_m * n_m x n = n_u x n
 
+% コストに正規化分を追加
+J += lambda / 2 * sum(sum(Theta .^ 2));
+J += lambda / 2 * sum(sum(X .^ 2));
 
-
-
-
-
-
-
-
-
-
+% gradに正規化分を追加
+X_grad += lambda * X;
+Theta_grad += lambda * Theta;
 
 % =============================================================
 
